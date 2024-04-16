6ddea3abdaa26693dbf824e27735f10409976e1c
Andrew Ferrouolo <andrew.ferruolo@gmail.com>
2024-04-16 14:56:56 -0400

    Issues syncing

---
+++ distributed_trainer.py
@@ -0,0 +1,102 @@
+import torch
+from torch import cuda
+from multiprocessing import Process, Queue
+from torch.utils.data import DataLoader
+
+
+# Adapting a similar approach to the end goal
+# for Stainless!
+
+def create_interfaces(synchronizer: [(Queue, Queue)], idx: int):
+    def getter():
+        return synchronizer[idx][0].get()
+
+    def setter(item):
+        synchronizer[idx][1].put(item)
+
+    return getter, setter
+
+
+def distributed_trainer(
+        model_list,
+        training_set,
+        test_set,
+        training_func,
+        training_config
+):
+    print("======Began Training=======")
+
+    # Kinda a hacky implementation of message passing
+    # but its what we got here
+    # Channels are indexed by device number. First channel
+    synchronizer = list()
+
+    threads = list()
+
+
+    device_batch_size = training_config.batch_size
+    total_batch_size = training_config.batch_size
+
+    train_loader = DataLoader(training_set, batch_size=training_config.batch_size * 4)
+    test_loader = DataLoader(test_set, batch_size=training_config.batch_size * 4)
+
+    for i in range(cuda.device_count()):
+        synchronizer.append((Queue(), Queue()))
+        (getter, setter) = create_interfaces(synchronizer, i)
+
+        training_process = Process(target=training_func, args=(
+            model_list[i],
+            i,
+            getter,
+            setter,
+            training_config
+        ))
+        training_process.start()
+        threads.append(training_process)
+
+    total_loss = 0
+    for epoch in range(training_config.num_epochs):
+        print(f"Training Epoch number {epoch}")
+        epoch_loss = 0
+
+
+
+
+
+
+
+
+        for i in range(cuda.device_count()):
+            train_loader = DataLoader(training_set, batch_size=training_config.batch_size, shuffle=True)
+            setter((train_loader, True))  # True indicates training mode
+
+            # Receive and accumulate the loss from the process
+            loss = getter()
+            epoch_loss += loss
+
+        total_loss += epoch_loss / cuda.device_count()
+        print(f"Epoch {epoch} loss: {epoch_loss / cuda.device_count()}")
+
+    # Evaluate the models on the test set
+    for i in range(cuda.device_count()):
+        (getter, setter) = create_interfaces(synchronizer, i)
+        test_loader = DataLoader(test_set, batch_size=training_config.batch_size, shuffle=False)
+        setter((test_loader, False))  # False indicates evaluation mode
+
+        # Receive and accumulate the test loss from the process
+        test_loss = getter()
+        total_loss += test_loss
+
+    print(f"Total loss: {total_loss / cuda.device_count()}")
+
+    # Terminate the processes
+    for i in range(cuda.device_count()):
+        (getter, setter) = create_interfaces(synchronizer, i)
+        setter(None)  # Send a signal to terminate the process
+
+    for process in threads:
+        process.join()
+
+    print("======Training Complete=======")
+
+# This is an example training function that should be replaced with your actual training logic
--- llama/llama_trainer.py
+++ llama/llama_trainer.py
@@ -1,45 +1,44 @@
 from torch import optim
-from torch.nn.loss import CrossEntropy
+import torch
+from torch.nn import CrossEntropyLoss
 
 
 class TrainerArgs:
-    learning_rate=1e-4
-    optim_params={}
-    optimizer=optim.Adam
-    loss=loss
+    learning_rate = 1e-4
+    optim_params = {}
+    optimizer = optim.Adam
+    loss_func = CrossEntropyLoss
+    num_epochs = 5
+
+
+def example_training_func(model, device_id, getter, setter, training_config: TrainerArgs):
+    model.cuda(device_id)
+    optimizer = training_config.optimizer(model.parameters(), lr=training_config.learning_rate)
+    loss_func = training_config.loss_func()
+
+
+    for epoch in range(training_config.num_epochs):
+        (x, y_true), is_training,  = getter()
+        if is_training is False:
+            break
+
+        epoch_loss = 0
+
+        ### too slow to do this every time
+        x, y_true = x.cuda(device_id), y_true.cuda(device_id)
 
 
 
+        optimizer.zero_grad()
+        output = model(x)
+        loss = loss_func(output, y_true)
+        if is_training:
+            loss.backward()
+            optimizer.step()
+        epoch_loss += loss.item()
 
-def training_thread(model, get_item, put_item, training_config):
-    
-    loss_func = training_config.loss()
-
-    optimizer = training_config.optim(
-            model.parameters(), 
-            training_config.learning_rate
-            )
-
-
-
-    while True: # Start Epoch
-        optimizer.zero_grad() 
-        batch = get_item()
-
-        torch.forward(batch) 
-
-        losss = loss_func()
-
-        loss.backward()
-
-        put_item(model.grad)
-
-        model.grad = get_item()
-
-        optimizer.step()
-
-        put_item(loss.item())
+        if is_training:
+            setter(epoch_loss / len(train_loader))
+        else:
+            setter(epoch_loss / len(train_loader))
     return model
-
-
-
